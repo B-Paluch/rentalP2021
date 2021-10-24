d5cc2d0838cf0682252053acb41c1c6a64f011ff
misio <kartofel6223@gmail.com>
2021-10-24 15:26:48 +0200

    login,register form

--- client/src/Main.js
+++ client/src/Main.js
@@ -1,91 +1,16 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import Home from "./Home";
-import Stuff from "./Stuff";
+import About from "./About";
 import Contact from "./Contact";
+import NavBar from "./components/NavBar";
 
 class Main extends Component {
   render() {
     return (
       <HashRouter>
         <div>
-          <nav class="navbar navbar-expand-lg navbar-light bg-light">
-            <div class="container-fluid">
-              <h3>Serwis Wypożyczeń</h3>
-              <button
-                class="navbar-toggler"
-                type="button"
-                data-bs-toggle="collapse"
-                data-bs-target="#navbarSupportedContent"
-                aria-controls="navbarSupportedContent"
-                aria-expanded="false"
-                aria-label="Toggle navigation"
-              >
-                <span class="navbar-toggler-icon"></span>
-              </button>
-              <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                  <li>
-                    <NavLink to="/">Home</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/stuff">Stuff</NavLink>
-                  </li>
-                  <li>
-                    <NavLink to="/contact">Contact</NavLink>
-                  </li>
-                  <li class="nav-item dropdown">
-                    <a
-                      class="nav-link dropdown-toggle"
-                      href="#"
-                      id="navbarDropdown"
-                      role="button"
-                      data-bs-toggle="dropdown"
-                      aria-expanded="false"
-                    >
-                      Menu
-                    </a>
-                    <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Dodaj przedmiot
-                        </a>
-                      </li>
-                      <li>
-                        <a class="dropdown-item" href="#">
-                          Usuń przedmiot
-                        </a>
-                      </li>
-                    </ul>
-                  </li>
-                </ul>
-                <form class="d-flex">
-                  <input
-                    class="form-control me-2"
-                    type="search"
-                    placeholder="Szukaj"
-                    aria-label="Search"
-                  />
-                  <button class="btn btn-outline-success" type="submit">
-                    Szukaj
-                  </button>
-                </form>
-                <div>
-                  <button type="button" class="btn btn-default navbar-btn">
-                    Zaloguj się
-                  </button>
-                  <button type="button" class="btn btn-default navbar-btn">
-                    Zarejestruj się
-                  </button>
-                </div>
-              </div>
-            </div>
-          </nav>
-          <div className="content">
-            <Route exact path="/" component={Home} />
-            <Route path="/stuff" component={Stuff} />
-            <Route path="/contact" component={Contact} />
-          </div>
+          <NavBar />
         </div>
       </HashRouter>
     );
